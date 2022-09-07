a09f9dd0293cc5589940f93b78aebbe16c3ec2e2
thiiagoms <thiagom.devsec@gmail.com>
2022-09-07 10:43:24 -0300

    feat: format code style :art:

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
-const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
-const pokemonImg = id => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}`;
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const pokemonImg = (id) =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}`;
 
-const generatePokemonPromisses = () => Array(150).fill().map((_, index) => fetch(getPokemonUrl(index + 1)).then(response => response.json()));
+const generatePokemonPromisses = () =>
+  Array(150)
+    .fill()
+    .map((_, index) =>
+      fetch(getPokemonUrl(index + 1)).then((response) => response.json())
+    );
 
-const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+const generateHTML = (pokemons) =>
+  pokemons.reduce((accumulator, { name, id, types }) => {
+    let elementTypes = types.map((typeinfo) => typeinfo.type.name);
 
-        let elementTypes = types.map(typeinfo => typeinfo.type.name);
-
-        accumulator += `
+    accumulator += `
             <li class="card ${elementTypes[0]}">
-                <img class="card-image" alt="${name}" src="${pokemonImg(id)}.png">
+                <img class="card-image" alt="${name}" src="${pokemonImg(
+      id
+    )}.png">
                 <h2 class="card-title">${id} . ${name}</h2>
-                <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+                <p class="card-subtitle">${elementTypes.join(" | ")}</p>
             </li>
         `;
-        return accumulator;
-    }, '');
+    return accumulator;
+  }, "");
 
-const insertPokemonIntoPage = pokemons => {
-    const ul = document.querySelector('[data-js="pokedex"]');
-    ul.innerHTML = pokemons;
+const insertPokemonIntoPage = (pokemons) => {
+  const ul = document.querySelector('[data-js="pokedex"]');
+  ul.innerHTML = pokemons;
 };
 
 const pokemonPromisses = generatePokemonPromisses();
 
-Promise.all(pokemonPromisses)
-    .then(generateHTML)
-    .then(insertPokemonIntoPage);
+Promise.all(pokemonPromisses).then(generateHTML).then(insertPokemonIntoPage);
